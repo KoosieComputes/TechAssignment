34e0bf8a00ece84d1828a520b15cea51057bed9c
Cobus Theunissen <cobus246@gmail.com>
2023-01-24 20:03:07 +0200

    Added models Customer and Document

--- crm/models.py
+++ crm/models.py
@@ -6,3 +6,14 @@ import datetime
 
 
 class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField("date registered")
+
+
+class Document(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    filename = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    mime_type = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField()
